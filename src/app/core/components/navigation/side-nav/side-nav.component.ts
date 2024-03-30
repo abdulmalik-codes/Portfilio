209cb7b663f0bc2e209205ffe48d35de1676da07
Abdul-Malik <abdul-malik.mohamed@rain.co.za>
2024-03-30 02:56:48 +0200

    dump

--- src/app/core/components/navigation/side-nav/side-nav.component.ts
+++ src/app/core/components/navigation/side-nav/side-nav.component.ts
@@ -2,6 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import {
+  faCopy,
+  faCode,
+  faEnvelope
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
@@ -13,5 +19,26 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ]
 })
 export class SideNavComponent {
+
+  selectedItem = 'copyIcon'
+
+  navList = [
+    {
+      name: 'copyIcon',
+      value: faCopy
+    },
+    {
+      name: 'codeIcon',
+      value: faCode
+    },
+    {
+      name: 'envelopeIcon',
+      value: faEnvelope
+    },
+  ]
+
+  selectPage(name: string){
+    this.selectedItem = name
+  }
 
 }
